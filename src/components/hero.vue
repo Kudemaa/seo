8cc1ed1b4681f0d51a42e5ff262a44ac1a163ef2
Kudemaa <shynfx@gmail.com>
2023-01-23 18:36:04 +0100

    new

---
+++ src/components/hero.vue
@@ -0,0 +1,65 @@
+<template lang="">
+  <div class="hero-container">
+    <navBar />
+    <div class="hero-content">
+      <div class="hero-info">
+        <img src="../assets/hero-icon.svg" alt="" />
+        <h1>Sales Optimization & Data Analysis</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus
+        </p>
+        <button>VIEW DETAILS</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import navBar from "./navBar.vue";
+export default {
+  components: {
+    navBar,
+  },
+};
+</script>
+<style lang="scss">
+.hero-container {
+  background-image: url("../assets/main-background.png");
+  height: 100vh;
+  width: 100vw;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: white;
+  display: flex;
+  flex-direction: column;
+}
+.hero-content {
+  height: 100vh;
+  display: flex;
+  align-items: flex-start;
+}
+.hero-info {
+  margin-top: 5rem;
+  margin-left: 13rem;
+  width: 30rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+.hero-info button {
+  background: none;
+  border-radius: 30.5px;
+  border: none;
+  background-color: #fba60b;
+  padding: 0.8rem 3rem;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
+.hero-content h1 {
+  font-size: 52px;
+  font-weight: 100;
+}
+</style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,65 @@
+<template lang="">
+  <div class="nav-container">
+    <div class="nav-sections">
+      <div class="nav-section1">
+        <img src="../assets/Logo.svg" alt="" />
+      </div>
+      <div class="nav-section2">
+        <ul class="list-items">
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">PAGES</a></li>
+          <li><a href="#">SERVICES</a></li>
+          <li><a href="#">CASE STUDIES</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </div>
+      <div class="nav-section3">
+        <img src="../assets/search.svg" alt="" />
+        <img src="../assets/items.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.nav-container {
+  padding-top: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.nav-sections {
+  display: flex;
+  justify-content: space-between;
+  width: 70rem;
+  align-items: center;
+}
+.nav-section1 img {
+  padding-top: 10px;
+}
+.nav-section2 {
+  width: 25rem;
+}
+.nav-section3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 7rem;
+}
+.nav-section3 img {
+  cursor: pointer;
+}
+.list-items {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  align-items: center;
+}
+.list-items li a {
+  text-decoration: none;
+  color: white;
+  font-size: 12px;
+}
+</style>
