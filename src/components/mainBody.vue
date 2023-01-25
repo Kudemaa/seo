372f3139fc7bb92da2eeae4c812adbd45a932d5a
Kudemaa <shynfx@gmail.com>
2023-01-25 22:49:58 +0100

    edits on card

--- src/components/mainBody.vue
+++ src/components/mainBody.vue
@@ -3,8 +3,19 @@
     <v-container class="pa-16">
       <v-row dense>
         <v-col v-for="card in cards" :cols="3" align="center">
-          <v-card outlined height="300" max-width="220" class="mt-8">
-            <v-img :src="card.icon"></v-img>
+          <v-card
+            outlined
+            height="300"
+            max-width="220"
+            class="mt-8 d-flex flex-column justify-start"
+          >
+            <v-img
+              :src="card.icon"
+              max-height="112"
+              max-width="112"
+              justify-start
+              class="ma-3"
+            ></v-img>
             <v-card-title>{{ card.title }}</v-card-title>
             <v-card-text>{{ card.subtitle }}</v-card-text>
           </v-card>
@@ -18,42 +29,42 @@ export default {
   data: () => ({
     cards: [
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
       {
-        icon: "../assets/cards/Card-1.svg",
+        icon: "Card-1.svg",
         title: "Digital PR Link-Building",
         subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
       },
