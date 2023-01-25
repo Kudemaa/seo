db7264bd43b93d1ecafc7c2780eac265cb56ed94
Kudemaa <shynfx@gmail.com>
2023-01-25 20:38:53 +0100

    creating the cards in main body

---
+++ src/components/mainBody.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div class="body-container">
+    <v-container>
+      <v-row>
+        <v-col v-for="n in 8" :cols="4" align="center">
+          <v-card outlined max-width="220" height="300">
+            <v-card-title>Tonight's availability</v-card-title>
+            <v-card-text>Crister ronaldo sui</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss"></style>
