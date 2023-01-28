3c3b3ada11c82601a1eee6a58be8bd7fd6a645f3
Kudemaa <shynfx@gmail.com>
2023-01-28 16:35:47 +0100

    edits on card title and stuff

--- src/components/mainBody.vue
+++ src/components/mainBody.vue
@@ -1,24 +1,39 @@
 <template lang="">
   <div class="body-container">
     <v-container class="pa-16">
-      <v-row dense>
+      <v-row>
         <v-col v-for="card in cards" :cols="3" align="center">
-          <v-card
-            outlined
-            height="300"
-            max-width="220"
-            class="mt-8 d-flex flex-column justify-start"
-          >
-            <v-img
-              :src="card.icon"
-              max-height="112"
-              max-width="112"
-              justify-start
-              class="ma-3"
-            ></v-img>
-            <v-card-title>{{ card.title }}</v-card-title>
-            <v-card-text>{{ card.subtitle }}</v-card-text>
-          </v-card>
+          <v-hover v-slot="{ hover }">
+            <v-card
+              outlined
+              height="300"
+              max-width="220"
+              class="mt-8 d-flex flex-column justify-start"
+              :class="{ 'on-hover': hover }"
+            >
+              <v-img
+                :src="card.icon"
+                max-height="112"
+                max-width="112"
+                justify-start
+                class="ma-3"
+              ></v-img>
+              <div
+                class="cardText"
+                style="width: 9rem; text-align: left; margin-left: 20px"
+              >
+                <h1 style="font-size: 16px">{{ card.title }}</h1>
+              </div>
+              <v-card-text style="font-size: 10px; text-align: left">{{
+                card.subtitle
+              }}</v-card-text>
+              <v-card-actions>
+                <v-btn text color="purple accent-4" @click="reveal = true">
+                  Learn More
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
         </v-col>
       </v-row>
     </v-container>
@@ -31,45 +46,60 @@ export default {
       {
         icon: "Card-1.svg",
         title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-2.svg",
+        title: "Link Building & Content",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-3.svg",
+        title: "Maps Search Optimization",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-4.svg",
+        title: "Big Data Analysis",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-5.svg",
+        title: "Custom Email Design",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-6.svg",
+        title: "Monitoring Ranking",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-7.svg",
+        title: "Code Optimization",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
       {
-        icon: "Card-1.svg",
-        title: "Digital PR Link-Building",
-        subtitle: "Lorem Solem ipsum azul mo9ran a9chiche",
+        icon: "Card-8.svg",
+        title: "Nap Syndication",
+        subtitle:
+          "Lorem ipsum dolor sit amet adipiscing elit ut aliquam, purus sit amet luctus venenatis",
       },
     ],
   }),
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.v-card .v-card-text {
+  line-height: 0.8rem;
+}
+.v-card.on-hover {
+  background-color: red;
+}
+</style>
